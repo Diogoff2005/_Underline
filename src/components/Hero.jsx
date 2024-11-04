84de5bf55899a3c9c6386849e986a8a011eda012
Diogo Fonseca <a2023123860@alunos.estgoh.ipc.pt>
2024-11-04 20:47:44 +0000

    Fixed font size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,12 +21,12 @@ const Hero = () => {
             />
             {/* Hero Content */}
             <div className="relative z-10 text-white px-4 text-left max-w-[1200px] w-full">
-              <h1 className="text-5xl font-bold mb-4">{headline.title}</h1>
+              <h1 className="text-4xl font-bold mb-4">{headline.title}</h1>
               <p className="text-lg mb-6 break-words max-w-[400px]">
                 {headline.description}
               </p>
               <Link to={`/movie-details/${headline.id}`}>
-                <button className="bg-gray-900  hover:bg-gray-800  text-white font-semibold py-2 px-4 rounded-lg transition duration-300">
+                <button className="bg-gray-900  hover:bg-gray-800 text-m text-white font-semibold py-2 px-4 rounded-lg transition duration-300">
                   See details
                 </button>
               </Link>
