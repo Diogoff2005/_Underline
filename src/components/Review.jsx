3aedf3b27631c669c54f004ce2f99ed7d06b6ecf
Diogo Fonseca <a2023123860@alunos.estgoh.ipc.pt>
2024-11-04 20:47:44 +0000

    Fixed font size

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -2,12 +2,12 @@ const Review = ({ review }) => {
   return (
     <div className="">
       <div className="flex justify-between">
-        <h3 className="font-semibold  text-2xl">{review.title}</h3>
+        <h3 className="font-semibold  text-xl">{review.title}</h3>
         <p className="bg-gray-900 text-white w-8 h-8 flex items-center justify-center rounded">
           {review.rating}
         </p>
       </div>
-      <p className="font-bold text-xl py-2">{review.movie}</p>
+      <p className="font-bold text-m py-2">{review.movie}</p>
       <p>{review.text}</p>
       <p className="text-sm text-gray-500">
         {review.first_name} {review.last_name} - {review.email}
